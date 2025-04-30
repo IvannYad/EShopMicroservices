20da361a67de157accebb25204ca1dff48e90831
Ivan Yadchyshyn <yad7devadress@gmail.com>
2025-04-30 13:43:37 +0300

    Implment CQRS and Feature for Creating Product in Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = command.Adapt<Product>();
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
